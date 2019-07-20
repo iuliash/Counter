34fbdd39df1df7661c02f02510eade8302babdf0
Julia <patrik9756@gmail.com>
2019-07-20 20:25:28 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
+
+var i = 0;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="count">
+          <h1>{i}</h1>
+        </div>
+        <div>
+          <button className="App-buttons" onclick={incCounter()}>+1</button>
+          <button className="App-buttons" onclick={decCounter()}>-1</button>
+        </div>
+        <div>
+          <input className="App-input" type="text"></input>
+          <button className="App-buttons" onclick={addNum()}>Add</button>
+        </div>
       </header>
     </div>
   );
+}
+
+function incCounter() {
+  i++;
+}
+
+function decCounter() {
+
+}
+
+function addNum(){
+
 }
 
 export default App;
