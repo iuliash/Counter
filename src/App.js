5d10da03efde6c0f5b609907f6afed7675405043
Julia <patrik9756@gmail.com>
2019-07-25 16:52:38 +0300

    v2

--- src/App.js
+++ src/App.js
@@ -1,39 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component} from 'react';
 import './App.css';
 
-var i = 0;
+class Couner extends React.Component {  
+  constructor(){  
+    super();  
+    this.state = {  
+        count : 0  
+    }; 
+    this.incrementCounter = this.incrementCounter.bind(this);
+    this.decrementCounter = this.decrementCounter.bind(this); 
+    this.addNumber = this.addNumber.bind(this); 
+  }
+
+  incrementCounter() {
+    this.setState({
+      count : this.state.count + 1
+    })
+  }
+
+  decrementCounter() {
+    this.setState({
+      count : this.state.count - 1
+    })
+  }
+
+  addNumber(){
+    var text = document.getElementsByTagName("input")[0];
+    var val=parseInt(text.value);
+    this.setState({
+      count : this.state.count + val
+    })
+  }
+
+  render() {
+    return (
+      <div id="count">
+        <div>
+          <h1>{this.state.count}</h1>
+          <button className="App-buttons" onClick={this.incrementCounter}>+1</button>
+          <button className="App-buttons" onClick={this.decrementCounter}>-1</button>
+        </div>
+        <div>
+          <input className="App-input" type="text"></input>
+          <button className="App-buttons" onClick={this.addNumber}>Add</button>
+        </div>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div id="count">
-          <h1>{i}</h1>
-        </div>
-        <div>
-          <button className="App-buttons" onclick={incCounter()}>+1</button>
-          <button className="App-buttons" onclick={decCounter()}>-1</button>
-        </div>
-        <div>
-          <input className="App-input" type="text"></input>
-          <button className="App-buttons" onclick={addNum()}>Add</button>
-        </div>
+        <Couner />
       </header>
     </div>
   );
-}
-
-function incCounter() {
-  i++;
-}
-
-function decCounter() {
-
-}
-
-function addNum(){
-
 }
 
 export default App;
